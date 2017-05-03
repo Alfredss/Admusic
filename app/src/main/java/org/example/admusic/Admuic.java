5153d5298d63a6761345ee1a9c2bd3334b0f0ae6
alfredss <alfredss.24.7@gmail.com>
2017-05-03 07:55:59 -0500

    we achieve control music play from another application, through interface with buttons

--- app/src/main/java/org/example/admusic/Admuic.java
+++ app/src/main/java/org/example/admusic/Admuic.java
@@ -1,13 +1,50 @@
 package org.example.admusic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Admuic extends AppCompatActivity {
-
+    private Button play;
+    private Button pause;
+    private Button stop;
+    private Button next;
+    private Button previus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admuic);
+
+        play = (Button) findViewById(R.id.btnPlay);
+        pause = (Button) findViewById(R.id.btnPause);
+        stop = (Button) findViewById(R.id.btnStop);
+    }
+
+    public void play(View view){
+        Intent intent = new Intent("com.android.music.musicservicecommand");
+        intent.putExtra("command", "play");
+        sendBroadcast(intent);
+    }
+    public void pause(View view){
+        Intent intent = new Intent("com.android.music.musicservicecommand");
+        intent.putExtra("command", "pause");
+        sendBroadcast(intent);
+    }
+    public void stop(View view){
+        Intent intent = new Intent("com.android.music.musicservicecommand");
+        intent.putExtra("command", "stop");
+        sendBroadcast(intent);
+    }
+    public void next(View view){
+        Intent intent = new Intent("com.android.music.musicservicecommand");
+        intent.putExtra("command", "next");
+        sendBroadcast(intent);
+    }
+    public void previous(View view){
+        Intent intent = new Intent("com.android.music.musicservicecommand");
+        intent.putExtra("command", "previous");
+        sendBroadcast(intent);
     }
 }
